995171869b565ed6234d724bd440567cf25c4c86
miaoxiaoge <taketimeasafriend@gmail.com>
2017-08-09 22:06:58 +0800

    select list3

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :events
   resource :user
 
+
   namespace :admin do
     root "events#index"
     resources :events
+    resources :users
   end
 
   root "events#index"
